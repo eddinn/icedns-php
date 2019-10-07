9c8699c0c66c7a999b03515b386bd413416c325a
Edvin Dunaway <edvin@nomad.eddinn.net>
2019-10-07 19:28:27 +0000

    initial push

---
+++ icedns.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>IceDNS</title>
+</head>
+<body>
+<form action="" method="get">
+<b>IP tala:</b> </b><input type="text" name="ip" value="" />
+<input type="submit" name="x" value="Athuga" />
+</form>
+<?php
+# Gerum ekki neitt nema GET-breytan ip innihaldi eitthvað
+if( $_GET['ip'] != '' )
+{
+    # Prentum út horizontal rule (línu) til að aðskilja formið frá niðurstöðum
+    echo '<hr />';
+    # Skerum IP töluna niður eftir punktunum og snúum henni við
+    $arr_ip = array_reverse( explode( '.', $_GET['ip'] ) );
+    # Athugum hvort iptala.iceland.rix.is skili 127.1.0.1
+    if( gethostbyname( implode( '.', $arr_ip ) . '.iceland.rix.is' ) == '127.1.0.1' )
+        # Ef svo er, er IP-talan íslensk
+        echo $_GET['ip'] . ' er .is IP-tala!';
+    else
+        # Ef ekki, er IP talan ekki íslensk
+        echo $_GET['ip'] . ' er ekki .is IP-tala!';
+}
+?> 
+</body>
+</html>
